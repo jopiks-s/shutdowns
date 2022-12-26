6327f25b6a3d63fa4438ef732c351d3e6d587c9a
aboba <73075720+jopiks-s@users.noreply.github.com>
2022-12-26 13:25:34 +0200

    raw codepoints convertor

---
+++ test_dir/codepoints_convert.py
@@ -0,0 +1,22 @@
+import re
+from codecs import decode
+
+
+def unicode_escape(s):
+    # https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python#24519338
+    escape_pattern = re.compile(r'''
+            ( \\U........      # 8-digit hex escapes
+            | \\u....          # 4-digit hex escapes
+            | \\x..            # 2-digit hex escapes
+            | \\[0-7]{1,3}     # Octal escapes
+            | \\N\{[^}]+\}     # Unicode characters by name
+            | \\[\\'"abfnrtv]  # Single-character escapes
+            )''', re.UNICODE | re.VERBOSE)
+    return escape_pattern.sub(lambda m: decode(m.group(), 'unicode-escape'), s)
+
+
+if __name__ == "__main__":
+    with open("resp.txt", "r", encoding="utf-8") as raw_file, open("escaped_unicode.txt", "w", encoding="utf-8") as u_file:
+        for line in raw_file.readlines():
+            u_file.write(unicode_escape(line))
+
--- test_dir/save_unicode_codepoints.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("unicode.txt", "w", encoding="utf-8") as file:
-    ustr = "привет \u0df1"
-    file.write(ustr)
-    print(ustr)
